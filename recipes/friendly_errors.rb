458b6a53f297e84818368d08eaf6e25ffe7fb8b0
Matt Venables <mattv@cantinaconsulting.com>
2011-09-17 15:41:11 -0400

    Cleaner friendly_errors recipe

--- recipes/friendly_errors.rb
+++ recipes/friendly_errors.rb
@@ -2,5 +2,13 @@ gem "friendly_errors"
 
 after_bundler do
   say_wizard "Enabling FriendlyErrors"
-  inject_into_file 'app/controllers/application_controller.rb', "\n  include FriendlyErrors\n  use_friendly_errors", :after => /protect_from_forgery/
+  
+  inject_into_file "app/controllers/application_controller.rb", :after => /ActionController::Base/ do
+    "\n  include FriendlyErrors\n"
+  end
+  
+  inject_into_file "app/controllers/application_controller.rb", :after => /protect_from_forgery/ do
+    "\n  use_friendly_errors"
+  end
+  
 end
