d2cfe453cf48ea651cc8c116d9042a46ae1f7b55
Matt Venables <mattv@cantinaconsulting.com>
2011-09-17 15:41:26 -0400

    Make database_renamer more agnostic

--- recipes/database_renamer.rb
+++ recipes/database_renamer.rb
@@ -2,5 +2,5 @@ gem 'silent-postgres', :group => :development
 
 db_username = ask_wizard("What's your database username? (Leave blank to use the default)")
 unless db_username.blank?
-  gsub_file 'config/database.yml', "username: #{app_name}", "username: #{db_username}"
+  gsub_file 'config/database.yml', "username:", "username: #{db_username} #"
 end
